e46015db82c52d6ae190f0cced19aaef2a9c1bd1
ambikajo <ambikajoshi@gmail.com>
2022-04-19 23:56:09 +0530

    removed bindery script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,16 @@
-import * as Bindery from 'bindery'
+// import * as Bindery from 'bindery'
 
 
-Bindery.makeBook({ 
-    content: {
-        selector: '#charactercards',
-        url: '/chargrid',
-      },
-    pageSetup: {
-        size: { width: '4in', height: '6in' },
-        margin: { top: '8pt', inner: '8pt', outer: '8pt', bottom: '8pt' },
-      },
-      rules: [
-        Bindery.PageBreak({ selector: '.content-card', position: 'before' }),
-      ]
-  });
+// Bindery.makeBook({ 
+//     content: {
+//         selector: '#charactercards',
+//         url: '/chargrid',
+//       },
+//     pageSetup: {
+//         size: { width: '4in', height: '6in' },
+//         margin: { top: '8pt', inner: '8pt', outer: '8pt', bottom: '8pt' },
+//       },
+//       rules: [
+//         Bindery.PageBreak({ selector: '.content-card', position: 'before' }),
+//       ]
+//   });
